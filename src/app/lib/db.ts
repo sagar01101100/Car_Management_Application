2e8ab3a9627874122ed46707bc1f1f9dbbb0860b
sagar01101100 <sagar01101100@gmail.com>
2024-11-17 20:08:48 +0530

    new Version

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -2,28 +2,32 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-const connect = async () =>{
+const connect = async () => {
     const connectionState = mongoose.connection.readyState;
-   if(connectionState == 1){
-    console.log("Database is already connected");
-    return;
-   }
-
-   if(connectionState == 2){
-    console.log("Database is connecting...");
-    return;
-   }
-   try{
-     mongoose.connect(MONGODB_URI!,{
-        dbName : 'fossil_of_knight',
-        bufferCommands: true
-     });
-    console.log("Database is connected");
+    if (connectionState === 1) {
+        console.log("Database is already connected");
+        return;
     }
-    catch(error: any){
+
+    if (connectionState === 2) {
+        console.log("Database is connecting...");
+        return;
+    }
+
+    try {
+        await mongoose.connect(MONGODB_URI!, {
+            dbName: 'CMAdb',
+            bufferCommands: true
+        });
+        console.log("Database is connected");
+    } catch (error: unknown) {
         console.log("Error connecting to database");
-        console.log("Error :", error);
-        } 
-}
+        if (error instanceof Error) {
+            console.log("Error:", error.message);
+        } else {
+            console.log("An unknown error occurred:", error);
+        }
+    }
+};
 
 export default connect;
