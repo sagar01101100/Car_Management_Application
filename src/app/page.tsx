1c80b9f4b5a7438be4fb8a45b8f48e000d0c3dc5
sagar01101100 <sagar01101100@gmail.com>
2024-11-17 23:37:34 +0530

    successfully added chakra ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+'use client'
 
-export default function page() {
+import {
+  Box,
+  Container,
+  Heading,
+  Text,
+  Stack,
+  useColorModeValue
+} from '@chakra-ui/react'
+
+export default function Home() {
+  const bgColor = useColorModeValue('gray.50', 'gray.800')
+  const textColor = useColorModeValue('gray.800', 'gray.100')
+
   return (
-    <div>
-      Car Management Application
-    </div>
+    <Container maxW="container.xl" py={8}>
+      <Stack spacing={6}>
+        <Box 
+          bg={bgColor} 
+          p={6} 
+          borderRadius="lg" 
+          boxShadow="base"
+        >
+          <Heading 
+            as="h1" 
+            size="xl" 
+            color={textColor}
+            mb={4}
+          >
+            Car Management System
+          </Heading>
+          <Text 
+            color={textColor}
+            fontSize="lg"
+          >
+            Welcome to your comprehensive car management solution
+          </Text>
+        </Box>
+      </Stack>
+    </Container>
   )
 }
